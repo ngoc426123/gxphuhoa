22d8a82149d549edb6a6d12edfc7acd13a4d8e30
MinhNgoc <minhngoc.ith@gmail.com>
2024-10-17 10:49:14 +0700

    Fetch & Render data to calendar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import { useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import './App.css';
 import Calendar from './components/Calendar';
 import { months } from './constants/date';
 
+// ICON
+import Left_icon from './assets/images/angle-left.svg';
+
 function App() {
   // STATE
-  const [current] = useState({ month: 10, year: 2024 });
+  const [current, setCurrent] = useState({ month: 10, year: 2024 });
+  const [prayData, setPrayData] = useState([]);
   const textMonth = useMemo(() => {
     return months[current.month - 1];
   }, [current.month]);
 
+  // METHOD
+  const getPrayForUs = useCallback(async () => {
+    try {
+      const { month } = current;
+      const apiUrl = process.env.REACT_APP_API + '/' + month;
+      const options = {
+        method: 'GET'
+      }
+      const response = await fetch(apiUrl, options);
+      const data = await response.json();
+
+      setPrayData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [current]);
+
+  const handleEventPreDate = () => {
+    let currentMonth = current.month;
+    let currentYear = current.year;
+
+    if (currentMonth === 1) {
+      currentMonth = 12;
+      currentYear--;
+    } else {
+      currentMonth--;
+    }
+
+    setCurrent({ month: currentMonth, year: currentYear });
+  };
+
+  const handleEventNextDate = () => {
+    let currentMonth = current.month;
+    let currentYear = current.year;
+
+    if (currentMonth === 12) {
+      currentMonth = 1;
+      currentYear++;
+    } else {
+      currentMonth++;
+    }
+
+    setCurrent({ month: currentMonth, year: currentYear });
+  };
+
+  const onClickDay = (day, month) => {
+    console.log('day nè', day, month);
+  }
+
+  // SIDE EFFECT
+  useEffect(() => {
+    getPrayForUs();
+  }, [getPrayForUs]);
+
+  // RENDER
   return (
     <div className="App-pray-for-us">
       <div className='App-title'>Xin cầu cho các linh hồn</div>
-      <div className='App-desc'>'Chính lúc chết đi là khi vui sống muôn đời.'</div>
+      <div className='App-desc'>"Chính lúc chết đi là khi vui sống muôn đời."</div>
       <div className='App-date-header'>
         <div className='App-date-info'>
           <div className='App-date-info__year'>{current?.year || 0}</div>
           <div className='App-date-info__month'>{textMonth}</div>
         </div>
+        <div className='App-date-control'>
+          <button
+            className='App-date-control__cta --left'
+            onClick={handleEventPreDate}
+          >
+            <img src={Left_icon} alt=''/>
+          </button>
+          <button
+            className='App-date-control__cta --right'
+            onClick={handleEventNextDate}
+          >
+            <img src={Left_icon} alt=''/>
+          </button>
+        </div>
       </div>
-      <Calendar {...current} />
+      <Calendar
+        {...current}
+        prayData={prayData}
+        onClickDay={onClickDay}
+      />
     </div>
   );
 }
--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,11 +1,16 @@
 import { useMemo, useState } from "react";
 
-import "./style.css";
 import { weekdays } from "../../constants/date";
+
+// IMAGE
+import Unknown_person_img from "../../assets/images/Unknown_person.jpg";
+
+// STYLE
+import "./style.css";
 
 export default function Calendar(props) {
   // PROPS
-  const { month, year } = props;
+  const { month, year, prayData, onClickDay } = props;
 
   // STATE
   const [weekday] = useState(weekdays);
@@ -33,6 +38,36 @@ export default function Calendar(props) {
       }
     });
   }, [month, year]);
+  const onCalendarPrayData = useMemo(() => {
+    return prayData
+      .reduce((cum, cur) => {
+        const date = new Date(cur.yearOfDead);
+        const day = date.getDate();
+        const hasDay = cum.some(item => item.day === day);
+
+        if (hasDay) {
+          const shipIndex = cum.findIndex(item => item.day === day);
+          const shipFilter = cum.filter(item => item.day === day)[0];
+          const shipData = shipFilter.data;
+
+          shipData.push(cur);
+
+          cum[shipIndex] = { ...cum[shipIndex], data: shipData };
+        } else {
+          cum.push({ day, data: [cur] });
+        }
+
+        return cum;
+      }, [])
+      .sort((a, b) => a.day - b.day);
+  }, [prayData]);
+  const onTopData = useMemo(() => {
+    return listdayData.map(item => {
+      const filterCal = onCalendarPrayData.filter(it => it.day === item.day)[0];
+
+      return { ...item, ...filterCal };
+    });
+  }, [listdayData, onCalendarPrayData]);
 
   // RENDER
   return (
@@ -43,9 +78,25 @@ export default function Calendar(props) {
         ))}
       </div>
       <div className="calendar__days">
-        {listdayData && listdayData.map((day, index) => (
-          <div className="calendar__day">
+        {onTopData && onTopData.map((day, index) => (
+          <div
+            key={index}
+            className={`calendar__day ${day.data ? '--need-pray' : ''}`}
+            {...(day.data 
+              ? { onClick: () => onClickDay(day.day, month) } 
+              : {})
+            }
+          >
             <span className="calendar__day-name">{day?.day || ''}</span>
+            {day.data && (
+              <div className="calendar__peace-list">
+                {day.data.map(item => (
+                  <div key={item.positionID} className="calendar__peace-item">
+                    <img src={item.img || Unknown_person_img} alt=""/>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         ))}
       </div>
